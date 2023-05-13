07c448e8356232dfa898a03707165505d2f9a1a2
zyperr <ramerica200@gmail.com>
2023-05-12 23:16:50 -0300

    Se creo clase Card para crear componentes dinamicos para la seccion Trending Stays

--- build/index.js
+++ build/index.js
@@ -27,14 +27,6 @@ const cardItems = [
     img: "bg-switzerland",
   },
 ];
-//*This are classes used in funcion dinamCard()
-const cardClassesArray = {
-  containerClasses: "w-52 h-64 shadow-md rounded-lg",
-  imgClasses: "w-52 h-3/5 rounded-t-lg bg-cover bg-center",
-  txtContClasses: "w-full h-2/5 bg-secondary rounded-b-lg",
-  titleClasses: "text-white font-bold text-xl px-4 py-2",
-  descClasses: "text-white px-4 whitespace-nowrap",
-};
 
 //TODO dynamic card function
 /*
@@ -47,45 +39,71 @@ const cardClassesArray = {
     -This the card structure used on the project
 
     ---- Card structure ----
-          <div class="w-48 h-64 shadow-md rounded-lg">
-            <div class="w-full h-3/5 rounded-t-lg bg-norway bg-cover"></div>
-            <div class="w-full h-2/5 bg-secondary rounded-b-lg">
-              <p class="text-white font-bold text-xl px-4 py-2">Norway</p>
-              <p class="text-white px-4 whitespace-nowrap">
+          <div class="card">
+            <div class="card-image"></div>
+            <div class="card-details-green" || class="card-details-blue">
+              <p class="card-title">Norway</p>
+              <p class="card-decription">
                 Beautiful landscapes
               </p>
             </div>
           </div>
-
+    ----                ----
 */
-const dinamCard = (
-  div = "",
-  h1 = "",
-  p = "",
-  img = "",
-  classArray = undefined
-) => {
-  for (const item of cardItems) {
+const dinamCard = (div = "", h1 = "", p = "", img = "") => {
+  return cardItems.map((item) => {
     const cardContainer = document.createElement(div);
     const imgBg = document.createElement(div);
     const textContainer = document.createElement(div);
     const titleCard = document.createElement(h1);
     const descriptionCard = document.createElement(p);
+    cardContainer.classList.add("card");
+    imgBg.classList.add("card-image", item.img);
 
-    cardContainer.classList.add(...classArray.containerClasses.split(" "));
-    imgBg.classList.add(...classArray.imgClasses.split(" "), item.img);
-    textContainer.classList.add(...classArray.txtContClasses.split(" "));
-    titleCard.classList.add(...classArray.titleClasses.split(" "));
-    descriptionCard.classList.add(...classArray.descClasses.split(" "));
+    if (
+      item.titleCard.includes("Norway") ||
+      item.titleCard.includes("Yosemite") ||
+      item.titleCard.includes("Switzerland")
+    ) {
+      textContainer.classList.add("card-details-green");
+    } else {
+      textContainer.classList.add("card-details-blue");
+    }
+    titleCard.classList.add("card-title");
+    descriptionCard.classList.add("card-decription");
 
     titleCard.textContent = item.titleCard;
     descriptionCard.textContent = item.descCard;
+
     cardContainer.appendChild(imgBg);
     textContainer.appendChild(titleCard);
     textContainer.appendChild(descriptionCard);
     cardContainer.appendChild(textContainer);
     cardSlider.appendChild(cardContainer);
-  }
+  });
 };
-dinamCard("div", "h1", "p", null, cardClassesArray);
 
+class Cards extends HTMLElement {
+  constructor() {
+    super();
+    this.imageCard = this.getAttribute("data-image");
+    this.titleCard = this.getAttribute("data-title");
+    this.textCard = this.getAttribute("data-text");
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+    <div class="w-full h-96  my-3 relative">
+    <img class="absolute -z-10 h-full object-cover rounded-3xl shadow-md brightness-75" src=${this.imageCard} alt=${this.titleCard}>
+    <div class="py-7">
+    <h3 class="font-bold text-xl px-4 py-2 text-white">${this.titleCard}</h3>
+    <p class="mx-4 font-medium text-base leading-loose text-white">${this.textCard}</p>
+    </div>
+    </div>`;
+  }
+
+}
+
+customElements.define("app-card", Cards);
+
+dinamCard("div","h3","p")
